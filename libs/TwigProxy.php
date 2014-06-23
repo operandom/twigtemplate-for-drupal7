16bc6fef085ed7a4380e5003b91ed06dbc070bbf
operandom <dev@operandom.com>
2014-06-23 18:25:57 +0200

    Switch hide function to filter

--- libs/TwigProxy.php
+++ libs/TwigProxy.php
@@ -6,6 +6,7 @@ class TwigProxy {
 	
 	public static $loader;
 	public static $twig;
+	public static $cacheRoot;
 	
 	public function __construct()
 	{
@@ -14,23 +15,53 @@ class TwigProxy {
 			self::$loader = new \Twig_Loader_Filesystem(getcwd());
 		}
 		
+		if (!self::$cacheRoot)
+		{
+			self::$cacheRoot = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'cache';
+			
+			if (!file_exists(self::$cacheRoot))
+			{
+				mkdir(self::$cacheRoot);
+			}
+		}
+		
 		if (!self::$twig)
 		{
-			self::$twig = new \Twig_Environment(self::$loader, array(
+			$twig = new \Twig_Environment(self::$loader, array(
+			
+				'autoescape' => false,
 				//@TODO Add cache system
 				//'cache' => self::$cacheRoot,
 			));
 			
-			self::$twig->addFunction(new \Twig_SimpleFunction('t', 't'));
+			$twig->addFunction(new \Twig_SimpleFunction('t', 't'));
 			
-			self::$twig->addFunction(new \Twig_SimpleFunction('render', function($value) {
-				return render($value); // fix strict warning
+			$twig->addFunction(new \Twig_SimpleFunction('render', function ($element) {
+				return render($element);
 			}));
 			
-			self::$twig->addFunction(new \Twig_SimpleFunction('theme', 'theme' ));
+			$twig->addFunction(new \Twig_SimpleFunction('theme', 'theme' ));
+		
+			$twig->addFilter(new \Twig_SimpleFilter('hide', function () {
+				
+				$arguments = func_get_args();
+				$element = array_shift($arguments);
+								
+				foreach ($arguments as $key)
+				{
+					if (array_key_exists($key, $element))
+					{
+						unset($element[$key]);
+					}
+				}
+				
+				return $element;
+			}));
+			
+			self::$twig = $twig;
 		}
 	}
-	
+		
 	public function render($view, $values)
 	{
 		return self::$twig->render($view, $values);
